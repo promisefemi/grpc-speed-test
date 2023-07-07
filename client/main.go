8167b0250b58692e6f5d5e7f7d7f0cb642e9e3f9
Promise Femi <promisefemi90@gmail.com>
2023-07-07 16:42:04 +0100

    Added counter to cached

--- client/main.go
+++ client/main.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	pool "github.com/promisefemi/grpc-client-pool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"os"
@@ -27,28 +26,18 @@ func main() {
 
 	port := flag.String("port", "9000", "Port")
 	thread := flag.Int("t", 1, "Thread")
+	connToOpen := flag.Int("c", 1, "Thread")
 	env := flag.String("e", "", "Env")
 	flag.Parse()
 
-	poolConfig := &pool.PoolConfig{
-		MaxOpenConnection:     10,
-		MaxIdleConnection:     10,
-		ConnectionQueueLength: 1000,
-		NewClientDuration:     10 * time.Second,
-		//Address:               "cached:9009",
-		Address: fmt.Sprintf(":%s", *port),
-		ConfigOptions: []grpc.DialOption{
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
-			grpc.WithBlock(),
-		},
-	}
+	address := fmt.Sprintf(":%s", *port)
 	if *env != "" {
-		poolConfig.Address = fmt.Sprintf("cached:%s", *port)
+		address = fmt.Sprintf("cached:%s", *port)
 	}
 
-	fmt.Println(poolConfig.Address)
+	fmt.Println(address)
 	var deviceId int64 = 6248148189135751453
-	poolCon := pool.NewClientPool(poolConfig)
+	poolCon := newClientConn(*connToOpen, address)
 
 	ticker := time.NewTicker(1 * time.Second)
 	go func() {
@@ -57,8 +46,8 @@ func main() {
 			deviceCount.Lock()
 			fmt.Printf("Get RPC Count: %d -- TimeStamp: %d \n", deviceCount.currentCount, time.Now().Unix())
 			fmt.Printf("Total RPC Count: %d \n", deviceCount.totalCount)
-			fmt.Printf("Total Number of Connections in Use: %d \n", poolCon.GetNumberOfConnectionsInUse())
-			fmt.Printf("Total Number of Available Idle Connections: %d \n\n", poolCon.GetNumberOfIdleConnections())
+			fmt.Printf("Total Number of Connections in Use: %d \n", poolCon.getNumberOfConnectionsInUse())
+			fmt.Printf("Total Number of Available Idle Connections: %d \n\n", poolCon.getNumberOfIdleConnections())
 			deviceCount.entries = append(deviceCount.entries, deviceCount.currentCount)
 			deviceCount.currentCount = 0
 			deviceCount.Unlock()
@@ -77,7 +66,7 @@ func main() {
 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-signalChannel
-		ticker.Stop()
+		//ticker.Stop()
 		total := 0
 		for _, entry := range deviceCount.entries {
 			total += entry
@@ -89,7 +78,7 @@ func main() {
 	}
 }
 
-func rpcCall(pool *pool.ClientPool, deviceId int64, deviceCount *counter, i int) {
+func rpcCall(pool *connPool, deviceId int64, deviceCount *counter, i int) {
 	for {
 		_, err := makeCall(pool, deviceId)
 		if err != nil {
@@ -102,15 +91,80 @@ func rpcCall(pool *pool.ClientPool, deviceId int64, deviceCount *counter, i int)
 		}
 	}
 }
-func makeCall(pool *pool.ClientPool, deviceId int64) (*proto.Device, error) {
-	conn, err := pool.Get()
-	if err != nil {
-		return nil, fmt.Errorf("error unable to establish connection -- %s\n", err)
-	}
-	defer conn.Release()
-	cachedService := proto.NewCacheClient(conn.Conn)
+func makeCall(pool *connPool, deviceId int64) (*proto.Device, error) {
+	//fmt.Println("call is being made")
+	conn := pool.get()
+	defer conn.release()
+	cachedService := proto.NewCacheClient(conn.conn)
 	deviceInput := &proto.DeviceInput{
 		Id: deviceId,
 	}
 	return cachedService.GetDevice(context.Background(), deviceInput)
+}
+
+type conn struct {
+	pool *connPool
+	conn *grpc.ClientConn
+}
+
+type connPool struct {
+	sync.Mutex
+	connInUse     int
+	availableConn []*conn
+}
+
+func newClientConn(num int, address string) *connPool {
+	connPool := &connPool{}
+	for i := num; i > 0; i-- {
+		//fmt.Println(num)
+		duration := time.Second * 5
+		//timeout := time.After()
+		ctx, cancel := context.WithTimeout(context.Background(), duration)
+		newConn, err := grpc.DialContext(ctx, address,
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+			grpc.WithBlock())
+		cancel()
+		if err != nil {
+			panic(err)
+		}
+
+		connPool.availableConn = append(connPool.availableConn, &conn{
+			pool: connPool,
+			conn: newConn,
+		})
+	}
+	return connPool
+}
+
+func (cp *connPool) getNumberOfConnectionsInUse() int {
+	cp.Lock()
+	defer cp.Unlock()
+	return cp.connInUse
+}
+func (cp *connPool) getNumberOfIdleConnections() int {
+	cp.Lock()
+	defer cp.Unlock()
+	return len(cp.availableConn)
+}
+func (cp *connPool) get() *conn {
+	for {
+		cp.Lock()
+		if len(cp.availableConn) > 0 {
+			//fmt.Println("connection")
+			conn := cp.availableConn[0]
+			cp.availableConn = cp.availableConn[1:]
+			cp.connInUse++
+			cp.Unlock()
+			return conn
+		} else {
+			cp.Unlock()
+		}
+	}
+}
+
+func (c *conn) release() {
+	c.pool.Lock()
+	c.pool.connInUse--
+	c.pool.availableConn = append(c.pool.availableConn, c)
+	c.pool.Unlock()
 }
